d2280ac0b6e063bac9f5a06aee9124ddb518e5a6
David L. Bowman <david@theinnovationlab.dev>
2024-11-30 14:43:13 -0600

    added getUser

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { GraphQLClient } from "graphql-request";
+import getUser from "./src/gql/users/queries/getUser";
 import getUsers from "./src/gql/users/queries/getUsers";
-
 const client = new GraphQLClient("http://localhost:4000/graphql");
 
 getUsers({ client }).then(console.log);
+getUser({ client, id: "01937e82-848a-7000-a6fc-77a4ad480df9" }).then(
+	console.log,
+);
---
+++ src/gql/users/queries/getUser.ts
@@ -0,0 +1,34 @@
+import type { GraphQLClient } from "graphql-request";
+import type { User } from "../../types/generated";
+
+const GET_USER = `
+query User($id: ID!) {
+    user(id: $id) {
+        id
+        name
+    }
+}
+`;
+
+interface GetUserInterface {
+	client: GraphQLClient;
+	id: string;
+}
+
+export default async function getUser({ client, id }: GetUserInterface) {
+	try {
+		if (!client) {
+			throw new Error("GraphQL client is required");
+		}
+
+		const { user } = await client.request<{ user: User }>(GET_USER, { id });
+
+		if (!user) {
+			throw new Error("No users found in the response");
+		}
+
+		return user;
+	} catch (error) {
+		console.error(`Error fetching user: ${error}`);
+	}
+}
