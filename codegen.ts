e69c7430143c7c3e734f118ff396e6368075f424
David L. Bowman <david@theinnovationlab.dev>
2024-11-30 14:45:19 -0600

    reconstructred resolvers

--- codegen.ts
+++ codegen.ts
@@ -1,9 +1,9 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-	schema: "src/schema/**/*.graphql",
+	schema: "src/gql/schema/**/*.graphql",
 	generates: {
-		"./src/types/generated.ts": {
+		"./src/gql/types/generated.ts": {
 			plugins: ["typescript", "typescript-resolvers"],
 			config: {
 				contextType: "./context#Context",
--- src/gql/schema/user/resolvers.ts
+++ src/gql/schema/user/resolvers.ts
@@ -2,71 +2,65 @@ import { randomUUIDv7 } from "bun";
 import { db } from "../../../sqlite/database";
 import type { Resolvers, User } from "../../types/generated";
 
-export function getUsers(): User[] {
-	return db.query("SELECT * FROM users").all() as User[];
-}
-
-export function getUser(id: string): User {
-	return db
-		.query("SELECT * FROM users WHERE id = $id")
-		.get({ $id: id }) as User;
-}
-
-export function createUser(name: string): User {
-	const id = randomUUIDv7();
-	const now = new Date().toISOString();
-
-	return db
-		.query(`
-            INSERT INTO users (id, name, created_at, updated_at) 
-            VALUES ($id, $name, $createdAt, $updatedAt) 
-            RETURNING id, name, created_at as createdAt, updated_at as updatedAt
-        `)
-		.get({
-			$id: id,
-			$name: name,
-			$createdAt: now,
-			$updatedAt: now,
-		}) as User;
-}
-
-export function updateUser(id: string, name: string): User {
-	const user = getUser(id);
-
-	if (!user) throw new Error(`User with id ${id} not found`);
-
-	user.name = name;
-	user.updatedAt = new Date().toISOString();
-
-	db.query(`
-		UPDATE users SET name = $name, updated_at = $updatedAt WHERE id = $id
-	`).run({
-		$name: name,
-		$updatedAt: user.updatedAt,
-		$id: id,
-	});
-
-	return user;
-}
-
-export function deleteUser(id: string): User {
-	const user = getUser(id);
-
-	if (!user) throw new Error(`User with id ${id} not found`);
-
-	db.query("DELETE FROM users WHERE id = $id").run({ $id: id });
-
-	return user;
-}
-
 export const resolvers: Resolvers = {
 	Query: {
-		users: () => getUsers(),
-		user: (_, { id }) => getUser(id),
+		users: () => {
+			return db.query("SELECT * FROM users").all() as User[];
+		},
+		user: (_, { id }) => {
+			return db
+				.query("SELECT * FROM users WHERE id = $id")
+				.get({ $id: id }) as User;
+		},
 	},
 	Mutation: {
-		createUser: (_, { name }) => createUser(name),
-		updateUser: (_, { id, name }) => updateUser(id, name),
-		deleteUser: (_, { id }) => deleteUser(id),
+		createUser: (_, { name }) => {
+			const id = randomUUIDv7();
+			const now = new Date().toISOString();
+
+			return db
+				.query(`
+					INSERT INTO users (id, name, created_at, updated_at) 
+					VALUES ($id, $name, $createdAt, $updatedAt) 
+					RETURNING id, name, created_at as createdAt, updated_at as updatedAt
+				`)
+				.get({
+					$id: id,
+					$name: name,
+					$createdAt: now,
+					$updatedAt: now,
+				}) as User;
+		},
+		updateUser: (_, { id, name }) => {
+			const user = db
+				.query("SELECT * FROM users WHERE id = $id")
+				.get({ $id: id }) as User;
+
+			if (!user) throw new Error(`User with id ${id} not found`);
+
+			user.name = name;
+			user.updatedAt = new Date().toISOString();
+
+			db.query(`
+				UPDATE users SET name = $name, updated_at = $updatedAt WHERE id = $id
+			`).run({
+				$name: name,
+				$updatedAt: user.updatedAt,
+				$id: id,
+			});
+
+			return user;
+		},
+		deleteUser: (_, { id }) => {
+			const user = db
+				.query("SELECT * FROM users WHERE id = $id")
+				.get({ $id: id }) as User;
+
+			if (!user) throw new Error(`User with id ${id} not found`);
+
+			db.query("DELETE FROM users WHERE id = $id").run({ $id: id });
+
+			return user;
+		},
 	},
 };
