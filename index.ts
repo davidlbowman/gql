d450347e54f441cf7df3ee92e7f0d6331a3b2c7a
David L. Bowman <david@theinnovationlab.dev>
2024-11-30 14:14:15 -0600

    i can now create my client and query

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+import { GraphQLClient } from "graphql-request";
+import getUsers from "./src/lib/user/user";
+
+const client = new GraphQLClient("http://localhost:4000/graphql");
+
+getUsers({ client }).then(console.log);
